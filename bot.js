6f0429fc1edf35168c3bfab73ae8188d065daff6
Unreas0nable <unreas0nablerblx@gmail.com>
2017-06-23 12:35:56 -0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('The bot is online!');
+});
+
+client.login('MzI3ODQ0MzU3MDkwODM2NDgx.DC7QYA.5ng1JvoCUH_dVpkKwxmVnAjxuVU');
